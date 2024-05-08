35ffe6f0a27aecb11da3082504c269ff3dd0f9cf
hajtidev <deadend6464@gmail.com>
2024-05-08 10:34:12 +0200

    chore: Add useEffect hook to fetch blog posts

--- src/components/BlogPosts.js
+++ src/components/BlogPosts.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import classes from "./BlogPosts.module.css";
 async function fetchPosts() {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
@@ -7,12 +7,12 @@ async function fetchPosts() {
 }
 function BlogPosts() {
     const[loadedPosts, setLoadedPosts] = useState([]);
-    function fetchPostsHandler() {
-        fetchPosts().then((fetchedPosts) => { setLoadedPosts(fetchedPosts)});
-    }
+    useEffect(function(){
+        fetchPosts().then((fetchedPosts)=> setLoadedPosts(fetchedPosts));
+    });
+    
     return (
         <>
-        <button onClick={fetchPostsHandler}>Fetch Blog Posts</button>
         <ul className={classes.posts}>
             {loadedPosts.map((post) => (
                 <li key={post.id}>{post.title}</li>
